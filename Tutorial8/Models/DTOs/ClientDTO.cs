cc7fa003b2afb62be93661e394c12c2276293df0
Tofame <98185616+Toffim@users.noreply.github.com>
2025-05-08 14:58:42 +0200

    Fill TripDTO and add ClientDTO
    We will need those models to use with api.

---
+++ Tutorial8/Models/DTOs/ClientDTO.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Tutorial8.Models.DTOs;
+
+public class ClientDTO
+{
+    [Key]
+    public int Id { get; set; }
+    [MaxLength(120)]
+    public string FirstName { get; set; }
+    [MaxLength(120)]
+    public string LastName { get; set; }
+    [MaxLength(120)]
+    public string Email { get; set; }
+    [MaxLength(120)]
+    public string Telephone { get; set; }
+    [MaxLength(120)]
+    public string Pesel { get; set; }
+}
--- Tutorial8/Models/DTOs/TripDTO.cs
+++ Tutorial8/Models/DTOs/TripDTO.cs
@@ -1,13 +1,24 @@
-﻿namespace Tutorial8.Models.DTOs;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Tutorial8.Models.DTOs;
 
 public class TripDTO
 {
+    [Key]
     public int Id { get; set; }
+    [MaxLength(120)]
     public string Name { get; set; }
+    [MaxLength(220)]
+    public string Description { get; set; }
+    public DateTime DateFrom { get; set; }
+    public DateTime DateTo { get; set; }
+    public int MaxPeople { get; set; }
     public List<CountryDTO> Countries { get; set; }
 }
 
 public class CountryDTO
 {
+    [Key]
+    public int Id { get; set; }
     public string Name { get; set; }
 }
